b71d6c8b683720894df3205eab9c61268d59161e
Guanghui Liang <meetguanghuiliang@gmail.com>
2024-05-27 01:05:15 +0800

    ✨ Add DynamicHeightText

---
+++ Sources/SwiftUIEssentials/DynamicHeightText.swift
@@ -0,0 +1,50 @@
+//
+//  DynamicHeightText.swift
+//  SwiftUIEssentials
+//
+//  Created by 梁光辉 on 2024/5/27.
+//
+
+import SwiftUI
+
+@available(macOS 14.0, *)
+@available(iOS 15.0, *)
+public struct DynamicHeightText: View {
+    @State var contentHeight: CGFloat
+    @State var minHeight: CGFloat
+    @State var maxHeight: CGFloat
+    @Binding var content: String
+    
+    public init(minHeight: CGFloat, maxHeight: CGFloat, content: Binding<String>) {
+        self.contentHeight = minHeight
+        self.minHeight = minHeight
+        self.maxHeight = maxHeight
+        self._content = content
+    }
+    
+    public var body: some View {
+        ScrollView {
+            Text(content)
+                .lineLimit(nil)
+                .background {
+                    GeometryReader(content: { geometry in
+                        Color.clear.onChange(of: content) { oldValue, newValue in
+                            print(geometry.size.height)
+                            let geoHeight = geometry.size.height
+                            contentHeight = min(maxHeight, max(minHeight, geoHeight))
+                        }
+                    })
+                }
+        }
+        .frame(height: contentHeight)
+    }
+}
+
+#Preview {
+    if #available(macOS 14.0, *) {
+        DynamicHeightText(minHeight: 50, maxHeight: 100, content: .constant("hello"))
+    } else {
+        // Fallback on earlier versions
+        EmptyView()
+    }
+}
