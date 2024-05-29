7edfc6b2f05118faa3e23db6650b7c091fd993ee
Guanghui Liang <meetguanghuiliang@gmail.com>
2024-05-29 11:51:26 +0800

    Update DynamicHeightTextView.swift

--- SwiftUIEssentialsTest/SwiftUIEssentialsDemo/DynamicHeightTextView.swift
+++ SwiftUIEssentialsTest/SwiftUIEssentialsDemo/DynamicHeightTextView.swift
@@ -8,17 +8,25 @@
 import SwiftUI
 import SwiftUIEssentials
 
+fileprivate let totalText: String = "Welcome to the GitHub Privacy Statement. This is where we describe how we handle your “Personal Data”, which is information that is directly linked or can be linked to you. It applies to the Personal Data that GitHub, Inc. or GitHub B.V., processes as the “Data Controller” when you interact with websites, applications, and services that display this Statement (collectively, “Services”). This Statement does not apply to services or products that do not display this Statement, such as Previews, where relevant."
+
 struct DynamicHeightTextView: View {
     @State var text: String = ""
     @State var timer: Timer?
-    @State var count = 0
+    @State var count = -1
+    let totalCount = totalText.count
     var body: some View {
         DynamicHeightText(minHeight: 50, maxHeight: 200, content: $text)
         .onAppear(perform: {
             Task {
-                timer = Timer(timeInterval: 0.5, repeats: true, block: { timer in
+                timer = Timer(timeInterval: 0.05, repeats: true, block: { timer in
                     count += 1
-                    text = text + "\(count)"
+                    if count >= totalCount {
+                        timer.invalidate()
+                        return
+                    }
+                    let chr = Array(totalText)[count]
+                    text = text + String(chr)
                 })
                 RunLoop.current.add(timer!, forMode: .common)
                 timer?.fire()
@@ -26,12 +34,12 @@ struct DynamicHeightTextView: View {
         })
         .textSelection(.enabled)
         .multilineTextAlignment(.leading)
-        .frame(maxWidth: 50, alignment: .topLeading)
+        .frame(maxWidth: 200, alignment: .topLeading)
         .padding(.all, 4)
         .font(.system(size: 16))
-        .foregroundStyle(.red)
-        .background(.gray)
+        .foregroundStyle(.black)
         .clipShape(.rect(cornerRadius: 8))
+        .border(.black)
         .padding(EdgeInsets(top: 16, leading: 16, bottom: 23, trailing: 16))
     }
 }
